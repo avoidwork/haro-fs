22927085fbeaafb29b981272e2e5908276412ff7
Jason Mulligan <jason.mulligan@avoidwork.com>
2015-08-31 23:02:17 -0400

    Initial commit of code (WIP)

---
+++ README.md
@@ -0,0 +1,45 @@
+# haro-fs
+
+[![Join the chat at https://gitter.im/avoidwork/haro-fs](https://badges.gitter.im/Join%20Chat.svg)](https://gitter.im/avoidwork/haro-fs?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
+
+[![build status](https://secure.travis-ci.org/avoidwork/haro-fs.svg)](http://travis-ci.org/avoidwork/haro-fs)
+
+[Harō](http://haro.rocks) is a modern immutable DataStore built with ES6 features, which can be wired to an API for a complete feedback loop.
+It is un-opinionated, and offers a plug'n'play solution to modeling, searching, & managing data on the client, or server
+(in RAM). It is a [partially persistent data structure](https://en.wikipedia.org/wiki/Persistent_data_structure), by maintaining version sets of records in `versions` ([MVCC](https://en.wikipedia.org/wiki/Multiversion_concurrency_control)).
+
+***haro-fs*** is a persistent storage adapter, providing 'auto saving' behavior, as well as the ability to `save()` & `load()` the entire DataStore.
+
+If `store.key` is not set, the fail over "id" field will be `id`.
+
+### How to use
+Require the adapter & register it with `haro.register(key, fn)`. The key must match the `store.adapters` key.
+
+If `iv` is specified, the file(s) on disk will contain ciphers, to be deciphered when loaded back into `haro`.
+
+```javascript
+var haro = require('haro'),
+    store;
+
+// Configure a store to utilize the adapter
+store = haro(null, {
+  adapters: {
+    fs: {
+      directory: "/path/to/records/"
+      iv: "none"
+    }
+  }
+});
+
+// Register the adapter
+store.register('mongo', require('haro-fs'));
+
+// Ready to `load()`, `batch()` or `set()`!
+```
+
+### Encryption
+`haro-fs` supports writing & reading ciphers. Supported 
+
+## License
+Copyright (c) 2015 Jason Mulligan
+Licensed under the BSD-3 license
---
+++ src/index.js
@@ -0,0 +1,138 @@
+"use strict";
+
+const deferred = require("tiny-defer");
+const fs = require("fs");
+const mkdirp = require("mkdirp");
+const path = require("path");
+const tmp = require("os").tmpdir();
+const cipher = require("tiny-cipher");
+const none = "none";
+const empty = "";
+
+function dir (fp) {
+	let defer = deferred();
+
+	mkdirp(fp, function (e) {
+		if (e) {
+			defer.reject(e);
+		} else {
+			defer.resolve(true);
+		}
+	});
+
+	return defer.promise;
+}
+
+function isDir (fp) {
+	let defer = deferred();
+
+	fs.exists(fp, function (exists) {
+		if (!exists) {
+			defer.reject(new Error("Path not found"));
+		} else {
+			fs.lstat(fp, function (e, stats) {
+				if (e) {
+					defer.reject(e);
+				} else {
+					defer.resolve(stats.isDirectory());
+				}
+			});
+		}
+	});
+
+	return defer.promise;
+}
+
+function read (fp, enc, salt) {
+	let defer = deferred();
+
+	isDir(fp).then(function (dir) {
+		if (dir) {
+
+		} else {
+			fs.readFile(fp, function (e, data) {
+				let ldata;
+
+				if (e) {
+					defer.reject(e);
+				} else {
+					if (enc === none) {
+						ldata = data;
+					}
+
+					try {
+						defer.resolve(JSON.parse(ldata));
+					} catch (err) {
+						defer.reject(err);
+					}
+				}
+			});
+		}
+	}, function (e) {
+		defer.reject(e);
+	});
+
+	return defer.promise;
+}
+
+function write (fp, enc, salt, data) {
+	let defer = deferred();
+
+	isDir(fp).then(function (dir) {
+
+	}, function (e) {
+		defer.reject(e);
+	});
+
+	return defer.promise;
+}
+
+function del (fp) {
+	let defer = deferred();
+
+	isDir(fp).then(function (dir) {
+
+	}, function (e) {
+		defer.reject(e);
+	});
+
+	return defer.promise;
+}
+
+function adapter (store, op, key, data) {
+	let defer = deferred(),
+		record = key !== undefined,
+		fpEnc = store.adapters.fs.encryption || none,
+		fpSalt = store.adapters.fs.salt || none,
+		fpDir = store.adapters.fs.directory || tmp,
+		prefix = store.id,
+		lkey = record ? prefix + "_" + key : empty;
+
+	dir(fpDir).then(function () {
+		if (op === "get") {
+			read(path.join(fpDir, lkey), fpEnc, fpSalt).then(function (data) {
+				defer.resolve(data);
+			}, function (e) {
+				defer.reject(e);
+			});
+		} else if (op === "remove") {
+			del(path.join(fpDir, lkey)).then(function () {
+				defer.resolve(true);
+			}, function (e) {
+				defer.reject(e);
+			});
+		} else if (op === "set") {
+			write(path.join(fpDir, lkey), fpEnc, fpSalt, record ? data : store.toArray()).then(function () {
+				defer.resolve(true);
+			}, function (e) {
+				defer.reject(e);
+			});
+		}
+	}, function (e) {
+		defer.reject(e);
+	});
+
+	return defer.promise;
+}
+
+module.exports = adapter;
